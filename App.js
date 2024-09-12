f3bad8c5f5831c123fd1ca203fe23aef1e550dd0
zc97 <zecheng.chow@gmail.com>
2024-09-12 12:39:45 -0700

    added Textinput and TextField which shows input text

--- App.js
+++ App.js
@@ -1,14 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 import Header from './Components/Header';
+import { useState } from 'react';
 
 export default function App() {
   const appName = "Zecheng's App";
+  const [text, setText] = useState("");
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <Header app_name={appName} />
-      <Text>Test</Text>
+      <Header name={appName} />
+      <TextInput 
+        placeholder='something to type'
+        keyboardType='defualt' 
+        style={{borderBlockColor:'purple', borderBottomWidth:1}}
+        value={text}
+        onChangeText={function (changedText) {
+          setText(changedText)
+        } }/>
+      <Text>{text} </Text>
     </View>
   );
 }
--- Components/Header.js
+++ Components/Header.js
@@ -2,10 +2,9 @@ import { View, Text } from 'react-native'
 import React from 'react'
 
 export default function Header(props) {
-    // console.log(props);
     return (
         <View>
-            <Text>Welcome to {props.app_name}!</Text>
+            <Text>Welcome to {props.name}!</Text>
         </View>
     );
 }
