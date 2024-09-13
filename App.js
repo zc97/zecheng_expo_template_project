a2b67a9ffb375c8f4d5ab05d88db569a9eade886
zc97 <zecheng.chow@gmail.com>
2024-09-12 17:48:50 -0700

    updated the Input component for typing more than 3 characters

--- App.js
+++ App.js
@@ -10,7 +10,7 @@ export default function App() {
     <View style={styles.container}>
       <StatusBar style="auto" />
       <Header name={appName} />
-      <Input />
+      <Input ifFocus={true} />
     </View>
   );
 }
--- Components/Input.js
+++ Components/Input.js
@@ -3,19 +3,39 @@ import React from 'react'
 import { useState } from 'react';
 
 
-export default function Input() {
+export default function Input({isFocus}) {
     const [text, setText] = useState("");
+    const [showMessage, setShowMessage] = useState(false);
+
     return (
     <View>
         <TextInput 
-        placeholder='something to type'
-        keyboardType='defualt' 
-        style={{borderBlockColor:'purple', borderBottomWidth:1}}
-        value={text}
-        onChangeText={function (changedText) {
-            setText(changedText)
-        } }/>
-        <Text>{text}</Text>
+            placeholder='something to type'
+            keyboardType='defualt' 
+            style={{borderBlockColor:'purple', borderBottomWidth:1}}
+            value={text}
+            onChangeText={function (changedText) {
+                setText(changedText);
+                setShowMessage(false);
+            } }
+            autoFocus={isFocus}
+            onBlur={() => {
+                setShowMessage(true)}}
+        />
+        
+        {/* <Text>{text}</Text> */}
+
+        {text.length > 0 && !showMessage && (
+                <Text>
+                    Character count: {text.length}
+                </Text>
+            )}
+
+        {showMessage && (
+            <Text>
+                {text.length >= 3 ? 'Thank you' : 'Please type more than 3 characters'}
+            </Text>
+        )}
     </View>
     )
 }
