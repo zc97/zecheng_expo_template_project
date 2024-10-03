d7c0967058c0d1e866bec4aba323105cccb85259
zc97 <zecheng.chow@gmail.com>
2024-10-03 11:28:26 -0700

    created a native stack navigator and added the home screen to the stack

--- App.js
+++ App.js
@@ -2,11 +2,17 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import Home from './Components/Home'
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+// console.log(Stack)
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Home/>
+      <Stack.Navigator>
+        <Stack.Screen name='Home' component={Home}></Stack.Screen>
+      </Stack.Navigator>
     </NavigationContainer>
   )
 }
