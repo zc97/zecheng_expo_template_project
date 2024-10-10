3da6b80b88e40510c86b95418a619fedfdb66a7b
zc97 <zecheng.chow@gmail.com>
2024-10-10 11:18:22 -0700

    added pressable component nested the GoalItem so it can be pressed and navigate to detail screen

--- Components/GoalItem.js
+++ Components/GoalItem.js
@@ -1,42 +1,51 @@
-import { View, Text, StyleSheet, Button } from 'react-native'
+import { View, Text, StyleSheet, Button, Pressable } from 'react-native'
 import React from 'react'
 import { useNavigation } from '@react-navigation/native';
 
-export default function GoalItem({goalObj, deleteHandler}) {
-    const navigation = useNavigation()
 
-    function handleDelete() {
-        deleteHandler(goalObj.id);
-    }
-    
-    return (
-        <View key={goalObj.id}  style={styles.textContainer}>
-            <Text style={styles.text}>{goalObj.text}</Text>
-            <Button title='X' color="#292929" onPress={handleDelete}/>
-            <Button 
-                title='i' 
-                color="#292929" 
-                onPress={() => {
-                    navigation.navigate('Details', {pressedGoal: goalObj})
-                }}
-            />
-        </View>
-    );
+export default function GoalItem({ goalObj, deleteHandler }) {
+	const navigation = useNavigation()
+
+	function handleDelete() {
+		deleteHandler(goalObj.id);
+	}
+
+	return (
+		<View key={goalObj.id} style={styles.textContainer}>
+			<Pressable 
+				style = {styles.horizontal}
+				onPress={() => {
+						navigation.navigate('Details', { pressedGoal: goalObj })
+						}}
+			>
+				<Text style={styles.text}>{goalObj.text}</Text>
+				<Button title='X' color="#292929" onPress={handleDelete} />
+				<Button
+					title='i'
+					color="#292929"
+				/>
+			</Pressable>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-    text: {
-        color: "purple",
-        padding: 15,
-        fontSize: 20,
-      },
-      textContainer: {
-        backgroundColor: "#aaa",
-        borderRadius: 5,
-        // marginTop: 20,
-        marginHorizontal: 30,
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "center",
-      },
+	text: {
+		color: "purple",
+		padding: 15,
+		fontSize: 20,
+	},
+	textContainer: {
+		backgroundColor: "#aaa",
+		borderRadius: 5,
+		// marginTop: 20,
+		marginHorizontal: 30,
+		flexDirection: "row",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	horizontal: {
+		flexDirection: 'row',
+		alignItems: 'center',
+	}
 });
