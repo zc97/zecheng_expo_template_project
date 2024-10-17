f4afe25fcafc61d003bcf7c1fba931aadb2bdb36
zc97 <zecheng.chow@gmail.com>
2024-10-17 12:54:42 -0700

    added firebase database to home component which handles the input, delete and deleteAll

---
+++ Firebase/firestoreHelper.js
@@ -0,0 +1,30 @@
+import { addDoc, collection } from "firebase/firestore";
+import { database } from "./firebaseSetup";
+
+export async function writeToDB(data, collectionName) {
+  try {
+    const docRef = await addDoc(collection(database, collectionName), data);
+    return docRef
+  } catch (err) {
+    console.error("Write to DB: ", err);
+  }
+}
+
+export async function deleteFromDB(deleteId, collectionName) { 
+  try {
+    await deleteDoc(doc(database, collectionName, deleteId));
+  } catch (err) {
+    console.error("Delete from DB: ", err);
+  }
+}
+
+export async function deleteAllFromDB(collectionName) {
+  try {
+    const querySnapshot = await getDocs(collection(database, collectionName));
+    querySnapshot.forEach((docSnapshot) => {
+      deleteFromDB(docSnapshot.id, collectionName);
+    });
+  } catch (err) {
+    console.error("Delete all from DB: ", err);
+  }
+}
