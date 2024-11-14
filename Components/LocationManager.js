b4b4ec8b8d69ffdd0225b5d7410e577adeb2bca1
zc97 <zecheng.chow@gmail.com>
2024-11-14 12:15:26 -0800

    Store the location object and use Google Maps Static API to show the user’s location on static map image

--- Components/LocationManager.js
+++ Components/LocationManager.js
@@ -1,9 +1,10 @@
-import { Alert, Button, StyleSheet, Text, View } from 'react-native'
+import { Alert, Button, StyleSheet, Text, View, Image } from 'react-native'
 import React from 'react'
 import * as Location from 'expo-location'
 
 
 export default function LocationManager() {
+  const [location, setLocation] = React.useState(null);
   const [response, requestPermission] = Location.useForegroundPermissions();
 
   async function verifyPermisson() {
@@ -26,6 +27,7 @@ export default function LocationManager() {
         return;
       }
       const locationResponse = await Location.getCurrentPositionAsync();
+      setLocation(locationResponse.coords);
       console.log("Location Response: ", locationResponse)
     } catch (error) {
       console.log("Error in getting the location: ", error)
@@ -34,8 +36,16 @@ export default function LocationManager() {
   return (
     <View>
       <Button title="Locate Me" onPress={locateUserHandler} />
+      {location && 
+            <Image source={{ uri: `https://maps.googleapis.com/maps/api/staticmap?center=${location.latitude},${location.longitude}&zoom=14&size=400x200&maptype=roadmap&markers=color:red%7Clabel:L%7C${location.latitude},${location.longitude}&key=${process.env.EXPO_PUBLIC_API_mapKey}` }} style={styles.image} />
+}
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  image:{
+    width: 100,
+    height: 100
+  },
+})
